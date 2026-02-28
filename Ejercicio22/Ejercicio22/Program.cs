2e5543ecf27c26ad580d882601bab7cfe3fae9d5
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 13:10:17 -0600

    Se terminó el proyecto 22.

--- Ejercicio22/Ejercicio22/Program.cs
+++ Ejercicio22/Ejercicio22/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string contrasea = "";
+
+while (contrasea != "koala123")
+{
+    Console.WriteLine("Ingrese la contraseña: ");
+    Console.WriteLine();
+    contrasea = Console.ReadLine();
+   
+    Console.Clear();
+
+}
+Console.WriteLine("La constraseña es correcta. ");

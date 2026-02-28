7d0af962b819240b56614cfac75b4b5c72554418
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 11:46:16 -0600

    Se terminó el proyecto 19.

--- Ejercicio19/Ejercicio19/Program.cs
+++ Ejercicio19/Ejercicio19/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int multiplo = 1;
+
+Console.WriteLine("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Tabla del {numero}");
+Console.WriteLine();
+
+while (multiplo <= 10)
+{
+    
+    Console.WriteLine($"{numero} x {multiplo} = {numero*multiplo}");
+    multiplo++;
+
+}

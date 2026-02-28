491a4b5778442dd59492a02f9e6e7e6a4e0fe820
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 13:01:51 -0600

    Se terminó el proyecto 21.

--- Ejercicio21/Ejercicio21/Program.cs
+++ Ejercicio21/Ejercicio21/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int suma = 0;
+
+while (suma <= 100)
+{
+    Console.WriteLine("Ingrese un número: ");
+    Console.WriteLine();
+    int numero= int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    suma += numero;
+
+}
+
+Console.WriteLine("La suma llegó al limite de ser menor o igual a 100");

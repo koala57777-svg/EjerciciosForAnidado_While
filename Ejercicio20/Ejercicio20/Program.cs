1152facefef0d04ce0ef9a04a9ea691c83f209f0
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 12:56:53 -0600

    Se terminó el proyecto 20.

--- Ejercicio20/Ejercicio20/Program.cs
+++ Ejercicio20/Ejercicio20/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int restar = 1;
+int contador = 0;
+
+Console.WriteLine("Ingrese un número: ");
+Console.WriteLine();
+int numero = int.Parse(Console.ReadLine());
+int numeroOriginal = numero;
+Console.WriteLine();
+
+while (numero >= 0)
+{
+    numero = numeroOriginal - restar;
+    restar = restar * 10;
+
+    if (numero >= 0 || numero==-1)
+    {
+        contador = contador + 1;
+    }
+}
+Console.WriteLine($"El la cantidad de dígitos que tiene el número es: {contador}");

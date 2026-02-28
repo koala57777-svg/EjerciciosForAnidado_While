841209032c6dab4f87a6a6e19d567c36d6a19dd7
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 09:33:28 -0600

    Se terminó el proyecto 5.

--- Ejercicio5/Ejercicio5/Program.cs
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Tabla del {i}");
+    Console.WriteLine();
+    for(int j=1; j<=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {j*i}");
+    }
+    Console.WriteLine();
+}

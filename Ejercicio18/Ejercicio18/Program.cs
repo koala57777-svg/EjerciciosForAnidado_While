6157b6ef6569e8b01f5c9097e8c37a11e9e63207
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 13:37:05 -0600

    Proyecto 18.1

--- Ejercicio18/Ejercicio18/Program.cs
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -5,11 +5,12 @@ Console.WriteLine("Ingrese un número: ");
 int numero = int.Parse(Console.ReadLine());
 int numeroinicial = numero;
 
-while(numero!=-1 && (numeroinicial-restar)>=1)
+while(numero>=0 && (numeroinicial-restar)>=1)
 {
     
     numero=numero*(numeroinicial-restar);
     restar++;
-}
 
+}
+Console.WriteLine();
 Console.WriteLine($"El factorial del numero ingresado es: {numero}");

dc6f057d35f84b11940a0fdbccdbd96922c6b305
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 10:04:42 -0600

    Se terminó el proyecto 8.

--- Ejercicio8/Ejercicio8/Program.cs
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int numero = 0;
+
+for (int i=1; i<=4; i++)
+{
+    for( int j=1; j<=4; j++)
+    {
+        Console.Write($" {j + numero}");
+    }
+    Console.WriteLine();
+    numero += 1;
+}

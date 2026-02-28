4a8ff5137ce1e316d60f73bd2112dfd7d4cd0f3e
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 10:35:54 -0600

    Se terminó el proyecto 17.

--- Ejercicio17/Ejercicio17/Program.cs
+++ Ejercicio17/Ejercicio17/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int numero = 1;
+int suma = 0;
+Console.WriteLine("Presione 0 para terminar.");
+while (numero != 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese un número: ");
+    numero=int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        suma += 1;
+    }
+}
+Console.WriteLine($"Ha ingresado: {suma} numeros positivos");

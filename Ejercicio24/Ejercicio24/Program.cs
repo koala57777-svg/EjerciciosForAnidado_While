e6b2ee25ac1db131fd37818e05fe6024c0ce3b53
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 13:23:52 -0600

    Se terminó el proyecto 24.

--- Ejercicio24/Ejercicio24/Program.cs
+++ Ejercicio24/Ejercicio24/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Ingrese un número: ");
+Console.WriteLine();
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+while (numero <= 100)
+{
+       Console.Write($" {numero},");
+    numero += 5;
+
+}

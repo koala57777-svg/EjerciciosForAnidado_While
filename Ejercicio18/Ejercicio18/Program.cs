02c9dc46804c85b94124e07fbe02045adfb8b27d
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 11:40:14 -0600

    Se terminó el proyecto 18.

--- Ejercicio18/Ejercicio18/Program.cs
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+int restar = 1;
+Console.WriteLine();
+Console.WriteLine("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+int numeroinicial = numero;
+
+while(numero!=-1 && (numeroinicial-restar)>=1)
+{
+    
+    numero=numero*(numeroinicial-restar);
+    restar++;
+}
+
+Console.WriteLine($"El factorial del numero ingresado es: {numero}");

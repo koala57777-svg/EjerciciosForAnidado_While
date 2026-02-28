3d911bebe0775a9ddfed6bba1abe988e66149de3
Gerber Ramírez <koala57777@gmail.com>
2026-02-28 10:29:45 -0600

    Se terminó el proyecto  16.

--- Ejercicio16/Ejercicio16/Program.cs
+++ Ejercicio16/Ejercicio16/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int numero = 1;
+
+while (numero != 0)
+{
+    Console.WriteLine("Ingrese un número: ");
+    numero= int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Saliendo del programa. Se presiono 0");
